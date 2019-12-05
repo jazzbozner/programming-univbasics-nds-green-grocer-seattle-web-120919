920e0b7a7bf7b6a90a10d1e4845ac2895c600dbc
Jazz Bozner <jazzbozner@gmail.com>
2019-12-05 22:38:10 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -31,9 +31,7 @@ def consolidate_cart(cart)
 end
 
 def apply_coupons(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+  
 end
 
 def apply_clearance(cart)
