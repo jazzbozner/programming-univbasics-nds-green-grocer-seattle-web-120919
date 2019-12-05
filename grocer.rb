95d7204bcfdfd38325905223c8a72c48b16972fb
Jazz Bozner <jazzbozner@gmail.com>
2019-12-05 22:42:35 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -38,7 +38,8 @@ def apply_coupons(cart, coupons)
     cart_item_with_coupon = find_item_by_name_in_collection(couponed_item_name, cart)
     if cart_item && cart_item[:count] >= coupons[counter][:num]
       if cart_item_with_coupon
-        cart_item_with_coupon[:count] += 
+        cart_item_with_coupon[:count] += counpons[counter][:num]
+        
       end
     end
     counter += 1
