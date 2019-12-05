acce8ed9ae299f6d4cd795df3614a974ec66c477
Jazz Bozner <jazzbozner@gmail.com>
2019-12-05 22:28:11 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -11,10 +11,14 @@ def find_item_by_name_in_collection(name, collection)
 end
 
 def consolidate_cart(cart)
-  new_car = []
+  new_cart = []
   counter = 0
   while counter < cart.length do
-    
+    new_cart_item = find_item_by_name_in_collection([cart[counter][:item], new_cart)
+    if new_cart_item != nil
+      new_cart_item
+    end
+    counter += 1
 end
 
 def apply_coupons(cart, coupons)
