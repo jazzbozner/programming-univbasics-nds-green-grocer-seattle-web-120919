b52a997e3f08f2e8b08ba47abb90bb8efd2ef5e6
Jazz Bozner <jazzbozner@gmail.com>
2019-12-05 22:49:56 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,6 +1,5 @@
 def find_item_by_name_in_collection(name, collection)
   counter = 0
-  
   while counter < collection.length do
     if collection[counter][:item] == name
       return collection[counter]
