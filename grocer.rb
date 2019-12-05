f2231184c858e86677ce38e9d73f6215a4295439
Jazz Bozner <jazzbozner@gmail.com>
2019-12-05 22:42:18 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -37,6 +37,12 @@ def apply_coupons(cart, coupons)
     couponed_item_name = "#{coupons[counter][:item]} W/COUPON"
     cart_item_with_coupon = find_item_by_name_in_collection(couponed_item_name, cart)
     if cart_item && cart_item[:count] >= coupons[counter][:num]
+      if cart_item_with_coupon
+        cart_item_with_coupon[:count] += 
+      end
+    end
+    counter += 1
+  end
 end
 
 def apply_clearance(cart)
