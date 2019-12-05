f134a4e3aa64d667f70342e0bc9d8beb509e4b02
Jazz Bozner <jazzbozner@gmail.com>
2019-12-05 22:55:49 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -76,4 +76,9 @@ def checkout(cart, coupons)
  while counter < final_cart.length
  total += final_cart[counter][:price] * final_cart[counter][:count]
  counter += 1
+ end
+ if total > 100
+   total -= (total - 0.10)
+ end
+ total
 end
